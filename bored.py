71819ad55472126f0994abae500e57fed3305216
Akhil <145592032+AKA-Akhil@users.noreply.github.com>
2024-01-10 16:10:51 +0530

    I was just bored

---
+++ bored.py
@@ -0,0 +1,9 @@
+for i in range(1,15):
+    if i%6==0:
+        print("KarunyaUniversity")
+    elif i%3==0:
+        print("University")
+    elif i%2==0:
+        print("Karunya")
+    else:
+        print(i)
